5e797313920efd01d9b0e6c7cec26acd24396d2a
MANIMARAN-M <61526125+MANIMARAN-M@users.noreply.github.com>
2020-08-27 17:10:17 +0530

    Day-6

---
+++ app.js
@@ -0,0 +1,4 @@
+const designOne = document.getElementById("design-one-img")
+const designTwo = document.getElementById("design-two-img")
+const desigThree = document.getElementById("design-three-img")
+const designFour = document.getElementById("design-four-img")
